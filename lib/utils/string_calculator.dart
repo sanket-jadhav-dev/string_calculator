e25c8d22c93983b735dea3af5409023aa8c48936
Sanket Jadhav <sanket.jadhav.dev@gmail.com>
2025-12-30 01:14:27 +0530

    Add supports newline as delimiter

--- lib/utils/string_calculator.dart
+++ lib/utils/string_calculator.dart
@@ -2,7 +2,8 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final values = numbers.split(',');
+    final values = numbers.replaceAll('\n', ',').split(',');
+
     return values.map(int.parse).reduce((a, b) => a + b);
   }
 }
--- test/utils/string_calculator_test.dart
+++ test/utils/string_calculator_test.dart
@@ -18,4 +18,8 @@ void main() {
     final calculator = StringCalculator();
     expect(calculator.add('1,2,3,4'), 10);
   });
+  test('supports newline as delimiter', () {
+    final calculator = StringCalculator();
+    expect(calculator.add('1\n2,3'), 6);
+  });
 }
