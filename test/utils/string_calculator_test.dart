98e6685f3efe6b6ab25dd40ba5810b4b0cbfabad
Sanket Jadhav <sanket.jadhav.dev@gmail.com>
2025-12-30 01:04:10 +0530

    Add returns 0 for empty string

---
+++ test/utils/string_calculator_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator/utils/string_calculator.dart';
+
+void main() {
+  test('returns 0 for empty string', () {
+    final calculator = StringCalculator();
+    expect(calculator.add(''), 0);
+  });
+}
